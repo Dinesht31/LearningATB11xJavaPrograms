451624b016e11e2939944831a9494f350c56ee3a
Dinesh <dinesht31@gmail.com>
2025-03-16 13:56:37 +0530

    16-March-Table of 9

--- src/ex_02_Java_Basics_part2/Table_of_9.java
+++ src/ex_02_Java_Basics_part2/Table_of_9.java
@@ -4,17 +4,17 @@ public class Table_of_9 {
     public static void main(String[] args) {
 //        Table of 9
         System.out.println("Table of 9");
-        int a=9,b=18,c=27,d=36,e=45,f=54,g=63,h=72,i=81,j=90;
-        System.out.printf("9x1=%d\n",a );
-        System.out.printf("9x2=%d\n",b );
-        System.out.printf("9x3=%d\n",c );
-        System.out.printf("9x4=%d\n",d );
-        System.out.printf("9x5=%d\n",e );
-        System.out.printf("9x6=%d\n",f );
-        System.out.printf("9x7=%d\n",g );
-        System.out.printf("9x8=%d\n",h );
-        System.out.printf("9x9=%d\n",i );
-        System.out.printf("9x10=%d\n",j );
+        int table=9;
+        System.out.printf("9x1=%d\n",table );
+        System.out.printf("9x2=%d\n",table*2 );
+        System.out.printf("9x3=%d\n",table*3 );
+        System.out.printf("9x4=%d\n",table*4 );
+        System.out.printf("9x5=%d\n",table*5 );
+        System.out.printf("9x6=%d\n",table*6 );
+        System.out.printf("9x7=%d\n",table*7 );
+        System.out.printf("9x8=%d\n",table*8 );
+        System.out.printf("9x9=%d\n",table*9 );
+        System.out.printf("9x10=%d\n",table*10 );
 
     }
 
